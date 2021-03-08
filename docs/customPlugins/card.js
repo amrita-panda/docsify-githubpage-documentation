95499bc97c29f41ccea3392ce8bc4ddfc5cd0bdc
amrita-panda <amritapanda@digitaldots.ai>
2021-03-08 15:53:47 +0530

    plugin modified

--- docs/customPlugins/card.js
+++ docs/customPlugins/card.js
@@ -1,159 +1,92 @@
 (function(){
-   
-    //   return('<div class="card">',
-    //   '<img src="../_images/logo.png" alt="Avatar">',
-    //   '<div class="container">',
-    //   '<h4><b>Jane Doe</b></h4>', 
-    //     '<p>Interior Designer</p> ',
-    //   '</div>',
-    // '</div>')
-
-    // var createCardDiv = function createCardDiv(background) {
-    //     let cardDiv=document.createElement('div');
-    //     cardDiv.classList.add('card');
-    //     cardDiv.style.background=background ||'lightgray';
-    //     return cardDiv;
-   // }
-    // var createImage=function createImage(img){
-    //     var image=document.createElement('img');
-    //     image.setAttribute('src','../_images/logo.png');
-    //     image.src='../_images/logo.png';
-    //     image.style.height='50px';
-    //     image.style.width='50px';
-    //     image.style.alignItems='center';
-    //     return image;
-    // }
-    // var cardBody=function cardBody(){
-    //     var div=document.createElement('div');
-    //     div.classList='container';
-    //     return div;
-    // }
-
-    // var cardHeader=function cardHeader(){
-    //     var h1=document.createElement('h4');
-    //     let b=document.createElement('b')
-    //     h1.appendChild(b);
-       
-    //     // h1.nextSibling('<p>Interior Designer</p> ');
-    //     return h1;
-
-    // }
-    // cardBody().appendChild(cardHeader());
-
-     //var mainDiv=createCardDiv();
-    // mainDiv.appendChild(createImage());
-    // mainDiv.appendChild(cardBody());
-    
-//return mainDiv;
-// function install(hook) {
-//  hook.done
-// }
-
-
-
-
-
-    // Matches tab set by start/end comment
-    // 0: Match
-    // 1: Indent
-    // 2: Start comment: <!-- card:start -->
-    // 3: Labels and content
-    // 4: End comment: <!-- card:end -->
-    const commentReplaceMark = 'card:replace';
-    const regex = {
-        // 1: Replacement HTML
-        commentReplaceMarkup: new RegExp(`<!-- ${commentReplaceMark} (.*) -->`),
+    const commentReplaceMark = 'cards:replace';
+   const regex={
         codeMarkup: /(```[\s\S]*?```)/gm,
-        cardBlockMarkup: /[\r\n]*(\s*)(<!-+\s+card:\s*?start\s+-+>)[\r\n]+([\s|\S]*?)[\r\n\s]+(<!-+\s+card:\s*?end\s+-+>)/m,
-        cardTextMarkup: /[\r\n]*(\s*)<!-+\s+card:\s*(.*)\s+-+>[\r\n]+([\s\S]*?)[\r\n]*\s*(?=<!-+\s+card?:(?!replace))/m,
-        cardImageMarkup: /[\r\n]*(\s*)#{1,6}\s*[*_]{2}\s*(.*[^\s])\s*[*_]{2}[\r\n]+([\s\S]*?)(?=#{1,6}\s*[*_]{2}|<!-+\s+card:\s*?end\s+-+>)/m
-    }
 
-  function renderCardStage1(content){
+    // Matches tab replacement comment
+    // 0: Match
+    // 1: Replacement HTML
+        commentReplaceMarkup: new RegExp(`<!-- ${commentReplaceMark} (.*) -->`),
+        cardGroup:/[\r\n]*(\s*)(<!-+\s+cards:\s*?start\s+-+>)[\r\n]+([\s|\S]*?)[\r\n\s]+(<!-+\s+cards:\s*?end\s+-+>)/m,
+        cardsInfo:/[\r\n]*(\s*)<!+-+\s*cards:start\s*-+>[\r\n]*\s*INFO=[A-Za-z]+@[a-zA-Z]+-[0-9]/m,
+        allCards:/[\r\n]?(\s)*#{1,6}\s*[A-za-z]+\s*=\s*.*[a-zA-z0-9/*a-zA-z/.a-zA-z]+\s*,[\r\n]*(\s)*[A-Za-z=\s*,=\s*0-9]+/mg,
+   }
+
+  function renderCardsStage1(content){
     const codeBlockMatch   = content.match(regex.codeMarkup) || [];
     console.log(codeBlockMatch)
     const codeBlockMarkers = codeBlockMatch.map((item, i) => {
         const codeMarker = `<!-- ${commentReplaceMark} CODEBLOCK${i} -->`;
         console.log(codeMarker)
-        // Replace code block with marker to ensure card markup within code
+        console.log(codeBlockMarkers);
+        // Replace code block with marker to ensure tab markup within code
         // blocks is not processed. These markers are replaced with their
-        // associated code blocs after card have been processed.
+        // associated code blocs after tabs have been processed.
         content = content.replace(item, () => codeMarker);
-        console.log(content);
+       
         return codeMarker;
     });
-   // return codeBlockMarkers
-    console.log(codeBlockMarkers);
-    let cardBlockMatch; // eslint-disable-line no-unused-vars
-    cardBlockMatch = regex.cardBlockMarkup.exec(content)
-        console.log(cardBlockMatch);
-        let cardBlock  = cardBlockMatch[0];
-        let cardStartReplacement = '';
-        let cardEndReplacement   = '';
-        const hasCardText =  regex.cardTextMarkup.test(cardBlock);
-        const hasCardImage = regex.cardImageMarkup.test(cardBlock);
-        const cardBlockIndent = cardBlockMatch[1];
-        const cardBlockStart  = cardBlockMatch[2];
-        const cardBlockEnd    = cardBlockMatch[4];
-        console.log(cardBlock);
-        console.log(cardBlockIndent);
-        console.log(cardBlockStart);
-        console.log(cardBlockEnd)
-        console.log(hasCardText);
-        console.log(hasCardImage)
-        if(cardBlock){
-            cardStartReplacement = `<!-- ${commentReplaceMark} <div class="card"> -->`;
+        let cardBlockMatch; // eslint-disable-line no-unused-vars
+        cardBlockMatch = regex.cardGroup.exec(content)
+         //get All cards Details
+       let cards=content.match(regex.allCards)|| [];
+       let cardBlock  = cardBlockMatch[0];
+
+         let cardStartReplacement = '';
+         let cardEndReplacement   = '';
+         const cardBlockIndent = cardBlockMatch[1];
+        //  const cardBlockStart  = cardBlockMatch[2];
+        //  const cardBlockEnd    = cardBlockMatch[4];
+         if(cardBlock){
+            cardStartReplacement = `<!-- ${commentReplaceMark} <div> -->`;
             cardEndReplacement = `\n${cardBlockIndent}<!-- ${commentReplaceMark} </div> -->`;
             console.log(cardStartReplacement);
             console.log(cardEndReplacement);
         }
-        let cardMatch =(regex.cardTextMarkup.exec(cardBlock)? regex.cardTextMarkup.exec(cardBlock) : null) || (regex.cardImageMarkup.exec(cardBlock)? regex.cardImageMarkup.exec(cardBlock) : null)
-        console.log(cardMatch)
-        const cardTitle   = (cardMatch[2] || '[Card]').trim();
-                const cardContent = (cardMatch[3] || '').trim();
-                console.log(cardTitle);
-                console.log(cardContent.split("'"))
-                let cardInfo=cardContent.split("'")
-                let cardText=cardInfo[2].split("**")
-                console.log(cardText);
-     cardBlock = cardBlock.replace(cardBlock, () => [
-        '<div class="card" style="background-color:lightgray;">',
-          '<img src='+`"${cardInfo[1]}"`+' alt="Avatar" class="card-image">',
-          '<div class="container">',
-          '<h4><b>'+`${cardText[1]}`+'</b></h4>', 
-            '<p>Interior Designer</p> ',
-          '</div>',
-        '</div>'
-        ].join(''));
+     let cardDom= createCards(cards);
 
-        cardBlock = cardBlock.replace(cardBlockStart, () => cardStartReplacement);
-        cardBlock = cardBlock.replace(cardBlockEnd, () => cardEndReplacement);
-        content = content.replace(cardBlockMatch[0], () => cardBlock);
-        return content;
-  
+     content = content.replace(cardBlockMatch[0], () => cardDom);
+    return content
 }
-
-
-function docsifyCard(hook, vm) {
-    let hasCard = false;
-
-  return  hook.beforeEach(function(content) {
-      console.log(content);
-        hasCard = regex.cardBlockMarkup.test(content);
+ 
+function createCards(cards){
+      return(
+          "<div class='row'>"+
+               cards.map((card,index)=>{
+                   console.log(card);
+                   
+                   let cardDetail=card.split('=');
+                   let cardsize=cardDetail[3].trim();
+                   console.log(cardDetail[3])
+              return(  "<div class='col-sm-"+cardsize+"'>"+
+                "<div class='bg-primary text-white card'>"+
+                    "<img class='align-right' src='"+`${cardDetail[1].split(',')[0]}`+"' alt='"+`${index}`+"'>"+
+                    "</img>"+
+                    "<div class='card-body'>"+
+                    "<h5 class='card-title text-uppercase text-white'>"+cardDetail[2].split(',')[0]+"</h5>"+
+                    "<p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>"+
+                    "<button href='#' class='btn btn-success '>Go somewhere</button>"+
+                "</div>"+
+            "</div>"+"</div>")
+        })
+          +"</div>"
+      ).replace(/,/g," ")
+}
+function docsifyCards(hook,vm){
+      let hasCard=false;
+         hook.beforeEach(function(content) {
+         hasCard = regex.cardGroup.test(content);
         if (hasCard) {
-            console.log("hi")
-              content = renderCardStage1(content)
-        }
-        console.log(content)
-        return content;
-    });
-
- }
-
+                content=renderCardsStage1(content)
+          }
+          return content;
+        })
+}
 if (window ) {
     var dom = Docsify.dom;
     console.log(dom);
-    $docsify.plugins = [].concat(docsifyCard, $docsify.plugins);
-}
+    $docsify.plugins = [].concat(docsifyCards, $docsify.plugins);
+
+  }
 })()
 
--- docs/solutions/create-solution.md
+++ docs/solutions/create-solution.md
@@ -1,18 +1,41 @@
-<!--  -->
-
-  ### createSolution page
+   ### createSolution page
  ![Slider control rendered on form](../_images/create-solution-image.png "Slider control rendered on form")  
 
 - **Web**. To make the code component available from any web browser, select the Web option next to the component. Notice that setting the Web option includes rendering the component in web browsers on PCs, Macs, and mobile devices.
 
 - **Phone**. To make the code component available on phones running Dynamics 365 for phones, select the Phone option next to the component.
 
+ <!-- cards:start -->
+         INFO=row@cardTheme-1
 
-<!-- card:start -->
+        #### IMG= ../_images/logo.png ,
+        TXT= first card ,
+        SIZE= 3 ^
 
-    #### ** img='../_images/logo.png' ** 
-    Solution
+       #### IMG= ../_images/logo.png ,
+        TXT= second card ,
+        SIZE= 3 ^ 
+
+       #### IMG= ../_images/logo.png ,
+        TXT= third card ,
+        SIZE= 3 ^
+
+       #### IMG= ../_images/logo.png ,
+        TXT= fourth card ,
+        SIZE= 2 ^
+
+       #### IMG= ../_images/logo.png ,
+        TXT= fifth card ,
+        SIZE= 2 ^
 
 
+<!-- cards:end -->
 
-<!-- card:end -->
+ <!-- cards:start -->
+         INFO=row@cardTheme-1
+
+        #### IMG= ../_images/logo.png ,
+        TXT= sixth card ,
+        SIZE= 4 ^
+
+<!-- cards:end -->
